21c33efca872d6687360ad9911dce6fb2d685450
Aditya Kumar Singh <adityasinghssj1@gmail.com>
2020-07-09 15:35:20 +0530

    Menu addd.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,21 @@ import './Header.css'
 const Header = () => {
     return (
         <div className="header-body">
-            <section className="heading">
+            <div className="heading">
                 <div className="heading-icon">
-                    <img src="https://jobs.vahan.co/icon_min.jpg" alt="Job Title"/>
+                    <img src="https://jobs.vahan.co/icon_min.jpg" alt="Job Title" />
                 </div>
-                    <div className="heading-title">Job Finder</div>
-            </section>
+                <div className="heading-title-body">
+                    <div className="header-title">
+                        Job Finder
+                       </div>
+                </div>
+            </div>
+            <div className="menu">
+                <button className="chat">CHAT</button>
+                <button className="my-job">MY JOB</button>
+                <button className="profile">PROFILE</button>
+            </div>
         </div>
     )
 }
